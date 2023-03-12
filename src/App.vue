fff002b1dc3380943c34eec7f8e2219ce6c32c79
asdkf123 <lustean@gmail.com>
2023-03-13 02:02:04 +0900

    전역 css 변경

--- src/App.vue
+++ src/App.vue
@@ -55,17 +55,16 @@ export default {
 </script>
 
 <style>
-/*#app {*/
-/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
-/*  -webkit-font-smoothing: antialiased;*/
-/*  -moz-osx-font-smoothing: grayscale;*/
-/*  text-align: center;*/
-/*  color: #2c3e50;*/
-/*}*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
 body {
   margin : 0;
-  padding: 10px;
 }
 div {
   box-sizing: border-box;
